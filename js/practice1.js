102bcb747567c95ce5d98d0e20968c218fc4dfe9
yejin <yejinlee0707@gmail.com>
2023-07-13 14:35:47 +0900

    practice1 수정

--- js/practice1.js
+++ js/practice1.js
@@ -2,16 +2,18 @@
 // 오브젝트 내에 인자(argument)를 받는 함수를 만들어서
 // 짝수인지 홀수인지 판별하게 해보세요.
 
-function isOdd(num) {
-  if (num % 2) {
-    console.log("홀수");
-  } else {
-    console.log("짝수");
-  }
-}
+myObject = {
+  num: 1000,
+  isOdd: function () {
+    if (this.num % 2) {
+      console.log("홀수");
+    } else {
+      console.log("짝수");
+    }
+  },
+};
 
-isOdd(2); // 짝수
-isOdd(3); // 홀수
+myObject.isOdd(); // 짝수
 
 // 2.
 // 배열 내 최대값 찾기
@@ -19,8 +21,14 @@ isOdd(3); // 홀수
 // 그 배열의 최대값을 반환해보세요.
 
 function findMaxNum(numArray) {
-  return Math.max(...numArray);
+  let maxNum = numArray[0];
+  for (i of numArray) {
+    if (i > maxNum) {
+      maxNum = i;
+    }
+  }
+  return maxNum;
 }
 
-const nums = [100000, 3, 3, 5, 6, 234, 3424, 11111, 333];
-console.log(findMaxNum(nums)); // 100000
+const nums = [99999, 3, 3, 5, 6, 234, 3424, 5000000, 333];
+console.log(findMaxNum(nums)); // 5000000

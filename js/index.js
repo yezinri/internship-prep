0a265f2dd1faed5a04a873307cf627e879567de1
yejin <yejinlee0707@gmail.com>
2023-07-13 17:37:29 +0900

    practice4

--- js/index.js
+++ js/index.js
@@ -1,56 +1,48 @@
-let a = 50;
-a = 30;
-a = 30.3;
-daysOfWeek = ["Monday", "Tuesday"];
+// let title = document.getElementById("title");
+// console.log(title.style.color);
+// title.style.color = "yellow";
+// title.innerText = "Hello JavaScript";
 
-myObject = {
-  name: "David",
-  age: 33,
-  favoriteMovies: ["Elemental", "The Matrix"],
-  favoriteFoods: [
-    {
-      name: "Pizza",
-      calories: 300,
-    },
-  ],
-  sayMyName: function () {
-    console.log(`My name is ${this.name}`);
-  },
-};
+// let obj = {
+//   name: "Ian",
+//   age: 34,
+// };
 
-function sayHello(name, age) {
-  console.log(`Hello ${name} you are ${age} years old`);
+// console.log(obj.name);
+// obj.name = "David";
+// console.log(obj.name);
+
+// document.title = "DOM Practice";
+
+// function notifyResize(event) {
+//   let width = window.innerWidth;
+//   let height = window.innerHeight;
+//   console.log(width, height);
+// }
+
+// window.addEventListener("resize", notifyResize);
+
+// const BASE_COLOR = "navy";
+// const OTHER_COLOR = "green";
+
+// function handleClick() {
+//   let currentColor = title.style.color;
+//   if (currentColor == BASE_COLOR) {
+//     title.style.color = OTHER_COLOR;
+//   } else {
+//     title.style.color = BASE_COLOR;
+//   }
+// }
+
+// title.addEventListener("click", handleClick);
+
+// 컬러가 4개 이상 들어가있는 배열을 만들고
+// 특정 이벤트를 통해 랜덤으로 컬러가 선택되도록 만들어보세요.
+
+const colors = ["pink", "yellow", "green", "purple", "orange"];
+
+function randomColorChange() {
+  title.style.color = colors[Math.floor(Math.random() * colors.length)];
 }
 
-sayHello("Yejin", 27);
-myObject.sayMyName();
-
-if (3 < 5) {
-  console.log("3 is smaller than 5");
-}
-
-for (let i = 0; i < 5; i++) {
-  console.log(i);
-}
-
-let array = ["사과", "배", "복숭아", "바나나", "딸기"];
-
-for (let i of array) {
-  console.log(i);
-}
-
-for (let i in array) {
-  console.log(array[i]);
-}
-
-let i = 0;
-while (i < 5) {
-  console.log("inside while loop");
-  i++;
-}
-
-let value = 0;
-do {
-  console.log(value);
-  value++;
-} while (value < 5);
+title.addEventListener("click", randomColorChange);

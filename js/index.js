6ce93419794302d5bc9ef6b1af479bbbfdd43c63
yejin <yejinlee0707@gmail.com>
2023-07-14 15:49:32 +0900

    practice5

--- js/index.js
+++ js/index.js
@@ -35,14 +35,3 @@
 // }
 
 // title.addEventListener("click", handleClick);
-
-// 컬러가 4개 이상 들어가있는 배열을 만들고
-// 특정 이벤트를 통해 랜덤으로 컬러가 선택되도록 만들어보세요.
-
-const colors = ["pink", "yellow", "green", "purple", "orange"];
-
-function randomColorChange() {
-  title.style.color = colors[Math.floor(Math.random() * colors.length)];
-}
-
-title.addEventListener("click", randomColorChange);
---
+++ js/practice5.js
@@ -0,0 +1,3 @@
+document.querySelector(".box").style.width = "200px";
+document.querySelector(".box").style.height = "200px";
+document.querySelector(".box").style.backgroundColor = "orange";
